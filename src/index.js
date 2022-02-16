8f0bdcf7708a60cb1876328acb849e6be10ad84f
Bryant Meskill <bryant.meskill@gmail.com>
2022-02-16 09:05:17 -0600

    Main logic in place.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
+import "./style.css";
+import renderHeader from "./header";
+import renderFooter from "./footer";
+import renderMain from "./frontPage";
+import renderMenu from "./menu";
+import renderContact from "./contact";
 
+const content = document.querySelector(".content");
+content.appendChild(renderHeader());
+content.appendChild(renderMain());
+content.appendChild(renderFooter());
+
+function render() {
+  const homeBtn = document.querySelector(".home");
+  const menuBtn = document.querySelector(".menu");
+  const contactBtn = document.querySelector(".contact");
+
+  homeBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(renderHeader());
+    content.appendChild(renderMain());
+    content.appendChild(renderFooter());
+    render();
+  });
+
+  menuBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(renderHeader());
+    content.appendChild(renderMenu());
+    content.appendChild(renderFooter());
+    render();
+  });
+
+  contactBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(renderHeader());
+    content.appendChild(renderContact());
+    content.appendChild(renderFooter());
+    render();
+  });
+}
+
+render();
+//removes child Nodes, but will leave behind event handlers, which can cause memory leak.
+//content.innerHTML = "";
