9175eaddadcdc00e6e67a150e7436a313f81f829
Bryant Meskill <bryant.meskill@gmail.com>
2022-02-16 09:05:51 -0600

    Added css-loader and style-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      //loading css files
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      //loading image files
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
