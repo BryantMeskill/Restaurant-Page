cb362faabacc692fe7c3d3cb253d2aa9579c5920
Bryant Meskill <bryant.meskill@gmail.com>
2022-02-18 08:38:18 -0600

    Added font support.

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      //loading font files
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
