f920f3c7fb558f915a1e79692500163860ec0c2b
Bryant Meskill <bryant.meskill@gmail.com>
2022-02-19 10:02:46 -0600

    Major styling changes.

--- src/frontPage.js
+++ src/frontPage.js
@@ -4,30 +4,30 @@ export default function renderMain() {
   const mainImage = document.createElement("img");
   const desc = document.createElement("p");
 
-  mainImage.classList.add("front-pic");
+  mainImage.classList.add("img");
   section.classList.add("section");
-  title.classList.add("title");
+  //title.classList.add("title");
   desc.classList.add("desc");
 
-  mainImage.src = "../src/spread.jpg";
+  mainImage.src = "../src/assets/img/spread.jpg";
   mainImage.alt = "A dish of chicken and rice.";
 
-  title.textContent = "Tres Amigos Mexican Restaraunt";
+  //title.textContent = "Tres Amigos Mexican Restaraunt";
   desc.textContent =
-    "Welcome to Tres Amigos Mexican restaraunt. We've been serving the\r\n";
+    "Welcome to Tres Amigos Mexican restaraunt. We've been serving the\n";
   desc.textContent +=
-    "local community delicious, authentic Mexican cuisine for over 40 years.\r\n";
+    "local community delicious, authentic Mexican cuisine for over 40 years.\n";
   desc.textContent +=
-    "We take pride in every customer served, and look forward to seeing you\r\n";
+    "We take pride in every customer served, and look forward to seeing you\n";
   desc.textContent +=
-    "and your family for many more years to come. Thanks for your continued\r\n";
+    "and your family for many more years to come. Thanks for your continued\n";
   desc.textContent +=
     "patronage! Check the menu and call us if you have any questions.";
 
   //setting this css style allows new line in textContent
   desc.setAttribute("style", "white-space: pre;");
 
-  section.appendChild(title);
+  //section.appendChild(title);
   section.appendChild(mainImage);
   section.appendChild(desc);
 
